fd501854a1e2dd869216b0e46091fd863e4b3e71
miseryangel <miseryangel1990@gmail.com>
2021-07-28 23:16:29 -0400

    preanimation

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -28,7 +28,7 @@ class CommentForm extends Component {
     handleSubmit(values) {
         this.toggleModal();
         alert(values.comment);
-        this.props.addComment(this.props.dishId,values.rating,values.author,values.comment);
+        this.props.postComment(this.props.dishId,values.rating,values.author,values.comment);
     }
 
     render(){
@@ -113,7 +113,7 @@ class CommentForm extends Component {
     }
 
 
-    function RenderComments({comments,addComment,dishId}){
+    function RenderComments({comments,postComment,dishId}){
         if (comments != null){
             const feedback = comments.map((post)=>{
                 return (
@@ -130,7 +130,7 @@ class CommentForm extends Component {
                     <ul className="list-unstyled">
                         {feedback}
                     </ul>
-                    <CommentForm dishId={dishId} addComment={addComment}/>
+                    <CommentForm dishId={dishId} postComment={postComment}/>
                 </div>
             );
         }else{
@@ -190,7 +190,7 @@ class CommentForm extends Component {
                     <div className="row">
                         <RenderDish dish={props.dish}/>
                         <RenderComments comments={props.comments} 
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             dishId={props.dish.id}/>
                     </div>
                 </div>
